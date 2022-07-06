45cdfe60f72635f512ea7a4ec6f032b077964061
Jacob Morris <jacobmorris405@gmail.com>
2022-07-06 10:11:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom/client'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+import {App} from './components'
+const root = ReactDom.createRoot(document.getElementById('app'));
+root.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>
+)
